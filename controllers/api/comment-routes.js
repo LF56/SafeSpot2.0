06b56c619412c9b976db3007119121fdb205702a
LF56 <leah.fox7@gmail.com>
2022-04-02 13:07:18 -0400

    code refactored

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -12,7 +12,8 @@ router.get('/', (req, res) => {
 });
 
 router.post("/", withAuth, (req, res) => {
-  if (req.session) {
+  console.log(req.body)
+  if (req.session.loggedIn) {
     Comment.create({
       comment_text: req.body.comment_text,
       post_id: req.body.post_id,
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -91,7 +91,9 @@ router.post('/', withAuth, (req, res) => {
 router.put('/:id', withAuth, (req, res) => {
   Post.update(
     {
-      title: req.body.title
+      title: req.body.title,
+      content: req.body.content,
+
     },
     {
       where: {
