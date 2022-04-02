2aa8365f4c9d924522550703aa223f464bc89c16
LF56 <leah.fox7@gmail.com>
2022-04-02 14:16:01 -0400

    maps in progress

--- public/javascript/fuzzysearch.js
+++ public/javascript/fuzzysearch.js
@@ -1,7 +1,31 @@
-const fuzzysearch = async() => {
-    let searchValue = document.getElementById('fuzzy').value
-    console.log(searchValue)
-}
- let buttonSearch = document.getElementById('search')
 
- buttonSearch.addEventListener('click', fuzzysearch)
+// const fuzzysearch = async () => {
+//     let searchValue = document.getElementById('fuzzy').value
+//     console.log(searchValue)
+// }
+// let buttonSearch = document.getElementById('search')
+
+// buttonSearch.addEventListener('click', fuzzysearch)
+
+async function mapFormHandler(event) {
+    event.preventDefault();
+    var searchMap = "hello"
+
+    if (searchMap) {
+        await fetch('/api/map', {
+            method: 'POST',
+            body: JSON.stringify({
+                searchMap
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then (
+            data => data.json()
+        ).then (
+            data => console.log(data)
+        )
+    }
+}
+
+
