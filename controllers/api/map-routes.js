db18fb399dccbc40df5ecbf6a964d3e1dfa563ce
LF56 <leah.fox7@gmail.com>
2022-04-03 16:37:02 -0400

    added images to main page, and link to hotline

--- controllers/api/map-routes.js
+++ controllers/api/map-routes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const axios = require('axios');
 
 require('dotenv').config();
-var mapRoute = `https://a.api.tomtom.com/map/1/tile/basic/main/1/0/0.png?key=${process.env.API_KEY}&tileSize=512`
+// var mapRoute = `https://a.api.tomtom.com/map/1/tile/basic/main/1/0/0.png?key=${process.env.API_KEY}&tileSize=512`
 var vectorRoute = `https://a.api.tomtom.com/map/1/tile/poi/main/1/0/0.png?key=${process.env.API_KEY}`
 
 router.post('/', (req, res) => {
