d4d931e9313671a6bbf9c6380731028905a327e4
LF56 <leah.fox7@gmail.com>
2022-04-02 23:05:24 -0400

    add a call to api

--- controllers/api/map-routes.js
+++ controllers/api/map-routes.js
@@ -3,15 +3,17 @@ const axios = require('axios');
 
 require('dotenv').config();
 var mapRoute = `https://a.api.tomtom.com/map/1/tile/basic/main/1/0/0.png?key=${process.env.API_KEY}&tileSize=512`
-
+var vectorRoute = `https://a.api.tomtom.com/map/1/tile/poi/main/1/0/0.png?key=${process.env.API_KEY}`
 
 router.post('/', (req, res) => {
-    axios.get(mapRoute).then (
-        data => 
-      {res.send(data.config)}
-      
-    )
-  });
+  axios.get(vectorRoute).then(
+    data => { 
+      console.log(data)
+      res.send(data.config) }
+
+  )
+});
+
 
 
 
