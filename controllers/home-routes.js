8b7040d595f44fc8657fbf6ecc23f6e97cc8cb0f
LF56 <leah.fox7@gmail.com>
2022-04-06 22:42:42 -0400

    added final changes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,7 +3,6 @@ const req = require('express/lib/request');
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-// get all posts for homepage
 router.get('/', (req, res) => {
   console.log('======================');
   Post.findAll({
@@ -43,7 +42,6 @@ router.get('/', (req, res) => {
     });
 });
 
-// get single post
 router.get('/post/:id', (req, res) => {
   Post.findOne({
     where: {
